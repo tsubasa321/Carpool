27ffdd290ebd472df7420445b417e6f19e4c561c
Alvin Jing <jingqi.hanzhong@gmail.com>
2016-06-18 06:30:32 -0300

    Initial Commit

---
+++ Carpool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carpool
+//
+//  Created by Qi (Alvin) Jing on 2016-06-18.
+//  Copyright © 2016 Qi (Alvin) Jing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
